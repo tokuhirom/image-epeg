000696b909c1ebc601356c40e7c017bcda5cff17
tokuhirom <tokuhirom@gmail.com>
2012-11-06 13:30:58 +0900

    Checking in changes prior to tagging of version 0.13.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index df55033..f9a0034 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension Epeg.
    
    +0.13
    +
    +    - Modernize package for better kwalitee
    +
     0.12
    
         - Win32 work fix, t/epeg_basic.t now passes on Win32(Corion)

--- lib/Image/Epeg.pm
+++ lib/Image/Epeg.pm
@@ -12,7 +12,7 @@ our @ISA = qw(Exporter DynaLoader);
 our %EXPORT_TAGS = ( 'constants' => [ qw(MAINTAIN_ASPECT_RATIO IGNORE_ASPECT_RATIO) ] );
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'constants'} } );
 our @EXPORT = qw();
-our $VERSION = '0.12';
+our $VERSION = '0.13';
 
 bootstrap Image::Epeg $VERSION;
 
